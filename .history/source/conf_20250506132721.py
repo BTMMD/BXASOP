d2c93d7dd55cf56aea0ac7a319801a2dd67d5e14
btmmd <btmmd@qq.com>
2025-05-06 13:05:47 +0800

    1 commit

--- .history/source/conf_20250506132721.py
+++ .history/source/conf_20250506132721.py
@@ -20,18 +20,6 @@ extensions = [
               "sphinx_copybutton", # 代码粘贴
 ]
 
-source_suffix = {
-    '.rst': 'restructuredtext',
-    '.txt': 'markdown',
-    '.md': 'markdown',
-} # 支持的文件类型
-
-myst_enable_extensions = [
-    "tasklist",
-    "deflist",
-    "dollarmath",
-]
-
 templates_path = ['_templates']
 exclude_patterns = []
 
@@ -41,24 +29,4 @@ language = 'zh_CN'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-
-html_theme_options = {
-    'analytics_anonymize_ip': False,
-    'logo_only': True,
-    #'display_version': True,
-    'prev_next_buttons_location': 'bottom',
-    'style_external_links': False,
-    'collapse_navigation': True,
-    'sticky_navigation': True,
-    'navigation_depth': 4,
-    'includehidden': True,
-    'titles_only': False,
-}
-
-html_static_path = ['_static','_images']
-
-html_logo = "./_images/logo.png"
-
-html_js_files = [
-    'my_custom.js',
-]
+html_static_path = ['_static']
---
+++ source/_static/my_custom.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    let div_logo = document.getElementsByClassName("wy-side-nav-search")[0];
+    let a_logo = div_logo.getElementsByTagName("a");
+    a_logo[0].href = "http://192.168.20.241:8090/index";
+    a_logo[0].target = "_blank";
+});
