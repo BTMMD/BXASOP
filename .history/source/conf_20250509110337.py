9cc454feed00277a264fdb576475eee11c3dcf93
btmmd <btmmd@qq.com>
2025-05-12 14:17:09 +0800

    3.3.1

---
+++ .history/source/conf_20250509110337.py
@@ -0,0 +1,110 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'BXASOP'
+copyright = '2025, itadmin'
+author = 'itadmin'
+release = 'Ver2025050601'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+              "myst_parser", # makedown
+              "sphinxcontrib.mermaid", # 
+              "sphinx_copybutton", # 代码粘贴
+              
+]
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+} # 支持的文件类型
+
+myst_enable_extensions = [
+    "tasklist",
+    "deflist",
+    "dollarmath",
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'zh_CN'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+
+html_theme_options = {
+    'analytics_anonymize_ip': False,
+    'logo_only': True,
+    #'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False,
+}
+
+html_static_path = ['_static']
+
+html_logo = "_static/images/logo.png"
+
+html_js_files = [
+    'my_custom.js',
+]
+
+
+html_css_files = [
+    'custom.css',
+]
+
+'''
+from docutils import nodes
+from docutils.parsers.rst import roles
+
+def new_window_link_role(name, rawtext, text, lineno, inliner, options={}, content=[]):
+    # 创建一个引用节点
+    reference = nodes.reference(rawtext, text, refuri=text, **options)
+    # 设置在新窗口打开
+    reference['target'] = '_blank'
+    return [reference], []
+
+# 注册自定义角色
+roles.register_local_role('newwin', new_window_link_role)
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- build/html/_static/my_custom.js
+++ build/html/_static/my_custom.js
@@ -1,6 +1,11 @@
-$(document).ready(function(){
+/* $(document).ready(function(){
     let div_logo = document.getElementsByClassName("wy-side-nav-search")[0];
     let a_logo = div_logo.getElementsByTagName("a");
     a_logo[0].href = "http://192.168.20.241:8090/index";
     a_logo[0].target = "_blank";
+}); */
+
+$(document).ready(function() {
+    $(".wy-side-nav-search a:first").attr("href", "http://192.168.20.241:8090/index");
+    $(".wy-side-nav-search a:first").attr("target", "_blank");
 });
